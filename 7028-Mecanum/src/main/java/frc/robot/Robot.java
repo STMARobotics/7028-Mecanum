f171374d09ff5c2c49e5e0184daabb938101743e
dylanrattai <817682@mystma.org>
2020-11-23 17:04:55 -0600

    Added Motor 0 Code

--- 7028-Mecanum/src/main/java/frc/robot/Robot.java
+++ 7028-Mecanum/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -21,6 +23,7 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private WPI_TalonSRX motor = new WPI_TalonSRX(0);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -89,6 +92,8 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+
+    motor.set(0.2230);
   }
 
   /**
