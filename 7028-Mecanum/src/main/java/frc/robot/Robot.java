febeb1f68d82bf37fd4183bb79ecdfed151b688a
dylanrattai <817682@mystma.org>
2020-11-23 17:21:57 -0600

    Added Motors

--- 7028-Mecanum/src/main/java/frc/robot/Robot.java
+++ 7028-Mecanum/src/main/java/frc/robot/Robot.java
@@ -7,6 +7,8 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -21,6 +23,10 @@ public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
   private RobotContainer m_robotContainer;
+  private TalonSRX leftFront = new TalonSRX(0);
+  private TalonSRX rightFront = new TalonSRX(1);
+  private TalonSRX leftRear = new TalonSRX(2);
+  private TalonSRX rightRear = new TalonSRX(3);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -31,6 +37,9 @@ public class Robot extends TimedRobot {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
     m_robotContainer = new RobotContainer();
+
+    leftRear.setInverted(true);
+    leftFront.setInverted(true);
   }
 
   /**
@@ -89,6 +98,11 @@ public class Robot extends TimedRobot {
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
+    
+    leftFront.set(1);
+    rightFront.set(1);
+    leftRear.set(1);
+    rightRear.set(1);
   }
 
   /**
